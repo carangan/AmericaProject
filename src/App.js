48d2d789b25c5d16f0e12b30e8f5aaf4b4e014c8
Janlloyd Carangan <malphene@gmail.com>
2020-03-06 17:25:49 -0600

    haha

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Bruh moment
+          Bruh moment is so cool
         </a>
       </header>
     </div>
