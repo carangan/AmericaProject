b179170396b5ed2f48e608137ee41d4f3a508495
jay-acosta <jayacosta@utexas.edu>
2020-03-07 12:30:57 -0600

    fixed organization 2

---
+++ front-end/src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      password: "",
+    }
+
+    this.click = this.click.bind(this);
+    this.handleChangeUsername = this.handleChangeUsername.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+  }
+
+  click() {
+    console.log(this.state.username, this.state.password);
+  }
+
+  handleChangeUsername(event) {
+    // console.log(event.target.value);
+
+    this.setState({
+      username: event.target.value
+    });
+  }
+
+  handleChangePassword(event) {
+    this.setState({
+      password: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>Hello!</p>
+        <div className="inputCon">
+          <input className="input-field" type="text" value={this.state.username} onChange={this.handleChangeUsername}></input>
+          <input className="input-field" type="password" value={this.state.password} onChange={this.handleChangePassword}></input>
+        </div>
+        <button onClick={this.click} className="helloBtn">Sign Up</button>
+      </div>
+    );
+  }
+}
+
+export default App;
