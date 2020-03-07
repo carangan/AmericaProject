f735cf3a900984612421c9874fb58ce065b79e30
Janlloyd Carangan <malphene@gmail.com>
2020-03-06 18:19:04 -0600

    cool stuff brotha

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <p>Hello!</p>
-      <input></input>
-      <input></input>
-      <button className="helloBtn">hello</button>
-    </div>
-  );
+class App extends Component {
+
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      password: "",
+    }
+
+    this.click = this.click.bind(this);
+    this.handleChangeUsername = this.handleChangeUsername.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+  }
+
+  click() {
+    console.log(this.state.username, this.state.password);
+  }
+
+  handleChangeUsername(event) {
+    // console.log(event.target.value);
+
+    this.setState({
+      username: event.target.value
+    });
+  }
+
+  handleChangePassword(event) {
+    this.setState({
+      password: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>Hello!</p>
+        <input type="text" value={this.state.username} onChange={this.handleChangeUsername}></input>
+        <input type="password" value={this.state.password} onChange={this.handleChangePassword}></input>
+        <button onClick={this.click} className="helloBtn">Sign Up</button>
+      </div>
+    );
+  }
 }
 
 export default App;
